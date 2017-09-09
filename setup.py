519c7f05d335d2ba8eedd0bdebf458e9ecb44a65
JhonnyBravo <sanfranceshika5@gmail.com>
2017-09-10 03:06:40 +0900

    modified: setup.py
    modified attribute: name, version, description, url, entry_points

--- setup.py
+++ setup.py
@@ -8,17 +8,21 @@ except IOError:
     long_description = ""
 
 setup(
-    name="python-template",
-    version="0.1",
-    description="Template of Python Project.",
+    name="pjs-automation",
+    version="1.0",
+    description="PhantomJS automation utility.",
     author="Jhonny Bravo",
     author_email="sanfranceshika5@gmail.com",
-    url="https://github.com/JhonnyBravo/python-template.git",
+    url="https://github.com/JhonnyBravo/pjs-automation.git",
     packages=find_packages(),
     install_requires=[],
     dependency_links=[],
     entry_points={
-        "console_scripts": []
+        "console_scripts": [
+            "get_content_by_css_selector=ps_phantomjs.bin.get_content_by_css_selector:main",
+            "get_content_by_tag_name=ps_phantomjs.bin.get_content_by_tag_name:main",
+            "get_content_by_xpath=ps_phantomjs.bin.get_content_by_xpath:main"
+        ]
     },
     long_description=long_description,
     classifiers=[
